f0df3dcb2a74d62f366e26c85c1d54ec2be1a0d4
Alírio Guerra <alirio@follow.55.com.br>
2021-10-17 20:36:15 -0300

    chore(index):start wp conversion

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,31 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('node-sass'));
 const autoprefixer = require('gulp-autoprefixer');
-
+const version = require('gulp-version-number');
 
 gulp.task('default', watch)
 gulp.task('sass', compilerSass)
+gulp.task('html', compilerHTML)
+
+function compilerHTML() {
+  return gulp.src('./*.html')
+  .pipe(version({
+    'value': '%MDS%',
+    'append': {
+      'key': 'v',
+      'to': ['css', 'js'],
+    }
+  }))
+  .pipe(gulp.dest('./'));
+}
 
 function compilerSass() {
   return gulp.src('./scss/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'})).pipe(autoprefixer({
-			cascade: false
-		}))
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./'));
+  .pipe(sass({outputStyle: 'compressed'})).pipe(autoprefixer({
+    cascade: false
+  }))
+  .on('error', sass.logError)
+  .pipe(gulp.dest('./'));
 }
 
 function watch() {
--- javascript/g26functions.js
+++ javascript/g26functions.js
@@ -1,29 +1,28 @@
-
 ScrollReveal().reveal('.headline', {
   reset: true,
   distance: '120%',
   origin: 'bottom',
   duration: 1000,
+  scrollBar:true,
   easing: 'cubic-bezier(0.5, 0, 0, 1)',
   interval: 1
 });
-
 
 (() => {
   window.addEventListener("scroll", () => {
     let scroll = this.scrollY;
     const el = document.getElementById('main-header');
-   if(scroll <= 100) {
-    el.classList.remove('scrolled')
-   } else {
-     el.classList.add('scrolled')
-   }
-});
+    if(scroll <= 100) {
+      el.classList.remove('scrolled')
+    } else {
+      el.classList.add('scrolled')
+    }
+  });
 })();
 
 
 const tl = gsap.timeline(), 
-    title = new SplitText("#hero-text", {type:"words"}), 
-    words = title.words;
+title = new SplitText("#hero-text", {type:"words"}), 
+words = title.words;
 
 tl.from(words, {duration: 1.2, opacity:0, x:-40, ease:"back", stagger: 0.2});
